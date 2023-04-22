fe7fc6a679d936946a4287ba4ea37592d303d990
Ivan Gallo <80438264+gustgallo@users.noreply.github.com>
2023-04-22 13:39:48 -0600

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,36 @@
-const body = document.querySelector("body");
-const navbar = document.querySelector(".menu");
-const menu = document.querySelector(".menu__list");
-const barBtn = document.querySelector(".menu__btn-bar");
-const cancelBtn = document.querySelector(".menu__btn-cancel");
-barBtn.onclick = () => {
-    menu.classList.add("active");
-    barBtn.classList.add("hide");
-    body.classList.add("disabledScroll");
-}
-cancelBtn.onclick = () => {
-    menu.classList.remove("active");
-    barBtn.classList.remove("hide");
-    body.classList.remove("disabledScroll");
-}
+//const body = document.querySelector("body");
+//const navbar = document.querySelector(".menu");
 
-window.onscroll = () => {
-    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky")
-}
+const button = document.querySelector('.button')
+const nav    = document.querySelector('.nav')
+
+
+button.addEventListener('click',()=>{
+    nav.classList.toggle('activo')
+})
+
+
+//const button = document.querySelector('.button')
+//const nav    = document.querySelector('.nav')
+
+//button.addEventListener('click',()=>{
+   //nav.classList.toggle('activo')
+//})
+
+
+
+//const cancelBtn = document.querySelector(".menu__btn-cancel");
+//barBtn.onclick = () => {
+  //  menu.classList.add("active");
+  //  barBtn.classList.add("hide");
+  // body.classList.add("disabledScroll");
+//}
+//cancelBtn.onclick = () => {
+   // menu.classList.remove("active");
+   //barBtn.classList.remove("hide");
+   //body.classList.remove("disabledScroll");
+//}
+
+//window.onscroll = () => {
+ //   this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky")
+//}
